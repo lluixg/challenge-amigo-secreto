3ae25682b53db007f70dac86939f2efe71dc1afb
Luiz Dias <lluizgustavodias@yahoo.com.br>
2025-09-23 01:39:26 -0300

    teste 3

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// Array para armazenar os amigos
 let amigos = [];
 
-// Função para adicionar amigo
 function adicionarAmigo() {
-    const input = document.getElementById("amigo");
-    const nome = input.value.trim(); // remove espaços extras
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
 
     if (nome === "") {
         alert("Digite um nome válido!");
@@ -19,34 +17,30 @@ function adicionarAmigo() {
 
     amigos.push(nome);
 
-    // Atualiza lista de amigos na tela
     atualizarLista();
 
-    // Limpa o campo
     input.value = "";
 }
 
-// Função para atualizar a lista de amigos no HTML
 function atualizarLista() {
-    const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // limpa lista antes de atualizar
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
 
     amigos.forEach((amigo) => {
-        const li = document.createElement("li");
+        let li = document.createElement("li");
         li.textContent = amigo;
         lista.appendChild(li);
     });
 }
 
-// Função para sortear amigo secreto
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert("Adicione pelo menos um amigo para sortear!");
         return;
     }
 
-    const sorteado = amigos[Math.floor(Math.random() * amigos.length)];
-    const resultado = document.getElementById("resultado");
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
 
     resultado.innerHTML = `<li>O amigo secreto sorteado é: <strong>${sorteado}</strong></li>`;
 }
